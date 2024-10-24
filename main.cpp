034b27faad406b6fe8fe52d0e658540efda2fdc8
TommasoAngiuli <tommaso.angiuli@marconibari.edu.it>
2024-10-24 08:41:50 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() 
+{
+    int n, i;
+    double numero, max;
+    cout << "inserisci il numero di valori da inserire" << endl;
+    cin >> n;
+    for (i = 1; i <= n; i++) {
+        cout << "inserisci il numero" << endl;
+        cin >> numero;
+        if (i == 1) {
+            max = numero;
+        }
+        if (numero > max) {
+            max = numero;
+        }
+    }
+    cout << "il numero massimo è " << numero << endl;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
